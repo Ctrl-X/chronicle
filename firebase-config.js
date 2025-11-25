9c3be7c9c436a8a64621c713597d291b48ff5c98
Julien Coupez <coupez@google.com>
2025-11-25 14:35:23 -0500

    added server files

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAtiSRdjmLVboFJMBrnBPQJsLnJDBDWVhQ",
+    authDomain: "vertexai-testing-453514.firebaseapp.com",
+    projectId: "vertexai-testing-453514",
+    storageBucket: "vertexai-testing-453514.firebasestorage.app",
+    messagingSenderId: "1037500942362",
+    appId: "1:1037500942362:web:b97f6815ce0f0e8d6529a3",
+    measurementId: "G-Z8CFJD5QTZ"
+};
